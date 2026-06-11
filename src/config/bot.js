1ee19a99af61085b502176e93f8fef8d5e6b8fba
kraz-cloud <davidolv166@gmail.com>
2026-06-11 14:07:26 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "fucking bored",
+        name: "i love to stalk people",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
